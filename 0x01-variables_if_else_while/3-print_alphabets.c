c51d3075631752688a65894dcecb829d838d4a79
Ayoola Adewole <adewole619@gmail.com>
2022-09-09 20:02:56 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+/**
+ * main - pint alphabet in lower case and
+ * then in  upper case
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+char letter
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(ltter);
+
+	putchar('\n')
+
+	return (0);
+
+}
