00f3e6c5db540a45013adb58f4cc1df366bc13c1
Ayoola Adewole <adewole619@gmail.com>
2022-09-09 21:16:58 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+/**
+ * main - -print lower case latter in reverse
+ *
+ * Return: Always 0.
+ */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; lettr >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
