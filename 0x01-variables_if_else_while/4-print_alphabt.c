efda47a09a457c32a975292ae5fc4e1455e1a77b
Ayoola Adewole <adewole619@gmail.com>
2022-09-09 20:45:29 -0700

    ask 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/**
+ *  main - -print alphabet in lowrcase
+ * except for q and e
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (etter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
