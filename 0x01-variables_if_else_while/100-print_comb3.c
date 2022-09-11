7e3c16e09a5895ee4f6daabbdba9df955740a152
Ayoola Adewole <adewole619@gmail.com>
2022-09-11 14:43:05 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(viod)
+{
+	int d = 48, a = 48;
+
+	while (d < 58)
+	{
+		a = 48;
+		while (a < 58)
+		{
+			if (d != a && d < a)
+			{
+				putchar(d);
+				putchar(a);
+
+				if (!(d == 56 && a == 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			a++;
+		}
+		d++
+	}
+	putchar('\n');
+
+	return (0);
+}
+
