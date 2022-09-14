152e25c852cb0efdb1a824a6bd17aa94dec2cce7
Ayoola Adewole <adewole619@gmail.com>
2022-09-13 20:00:02 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ *Return: always 0(Success)
+ *
+ */
+
+int main(void)
+
+{
+	char l[] = {"_putchar"};
+	int i;
+	char str[] = "_putchar";
+	int ch;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(l[i]);
+	}
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+
+}
