501f072d2d1db8cf2ac1bf02184776804e1f83c0
Ayoola Adewole <adewole619@gmail.com>
2022-09-18 11:13:43 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print _alphabet in loweer case
+ *
+ *Reture: Always 0.
+ */
+
+void print_alphabet(void);
+{
+	char r;
+
+	for (r = 'a'; r <= 'z'; r++)
+	{
+		_putchar(r);
+	}
+	_putchar('\n')
+}
