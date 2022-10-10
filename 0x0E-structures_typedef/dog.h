d6b97224467b0c3f6470e6ffaaafd94126306b26
Ayoola Adewole <adewole619@gmail.com>
2022-10-10 13:44:43 -0700

    task 012

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stddef.h>
 
 /**
  * struct dog - A new type describing a dog.
@@ -21,6 +22,7 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
 void free_dog(dog_t *d);
 
 #endif
