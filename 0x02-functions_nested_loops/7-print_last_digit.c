74fd615c1a122b66345ec7b0291905df32673b7b
Adewole619 <adewole619@gmail.com>
2023-09-12 17:15:06 -0700

    bewe no2

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /*
- * main - check last digit.
+ * main - print last digit.
  * Return: the last digit.
  */
 
 int print_last_digit(int n)
 {
 	int lastDig;
+
 	if (n >= 0)
 {
 		lastDig = n % 10;
