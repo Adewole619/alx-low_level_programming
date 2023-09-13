c653e9c2bccfef0a920beea96132f8101da2bc6d
Adewole619 <adewole619@gmail.com>
2023-09-12 17:10:43 -0700

    bewe

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,5 +7,15 @@
 
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int lastDig;
+	if (n >= 0)
+{
+		lastDig = n % 10;
+}
+	else
+{
+		lastDig = (n % 10) * -1;
+}
+	_putchar('0' + lastDig);
+return (lastDig);
 }
