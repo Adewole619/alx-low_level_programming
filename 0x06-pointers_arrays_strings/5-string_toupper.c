b801f2d5cecdd681d567ff9f8f54b7ca5c51d0ac
Adewole619 <adewole619@gmail.com>
2023-09-22 09:15:43 -0700

    lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,8 +13,9 @@ char *string_toupper(char *c)
 
 	while (c[i] != '\0')
 	{
-		if (c[i] > 96 && c[i] < 123)
-			c[i] -= 32;
+		if (c[i] >= 'a' && c[i] =< 'z')
+			c[i] = c[i] - 32;
+			i++;
 	}
 	return (c);
 }
